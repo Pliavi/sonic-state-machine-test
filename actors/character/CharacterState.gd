3948d7f4195487f81ff40589e16556e2aafe61b2
Pliavi <pliavi.rpgm@gmail.com>
2021-01-20 21:54:02 -0300

    create a character state class

--- actors/character/CharacterState.gd
+++ actors/character/CharacterState.gd
@@ -2,10 +2,32 @@ extends State
 
 class_name CharacterState
 
-func _physics_process(delta):
-	._physics_process(delta)
-	
-	var character = self.target
+var behaviors = []
+var _behaviors_refs = []
+
+
+func _ready():
+	._ready()
+	for behavior_name in self.behaviors:
+		var behavior_ref = funcref(
+			self._actor._behavior_instance, behavior_name
+		)
+		self._behaviors_refs.append(behavior_ref)
+
+
+func _enter(options: Dictionary = {}):
+	var sprite = self._actor.sprite as AnimatedSprite
+	sprite.play(self.name)
+	self.enter()
+
+func _update(delta):
+	var character = self._actor
+
+	for behavior in self._behaviors_refs:
+		behavior.call_func(delta, character)
+
+	self.update(delta)
+
 	character.velocity = character.move_and_slide(
 		character.velocity, Vector2.UP
 	)
