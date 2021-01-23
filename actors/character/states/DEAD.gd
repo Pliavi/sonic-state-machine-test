318f99729a2b12009e10f8fc189c19631c91da7c
Pliavi <pliavi.rpgm@gmail.com>
2021-01-23 15:53:52 -0300

    write dead and hurt states

--- actors/character/states/DEAD.gd
+++ actors/character/states/DEAD.gd
@@ -1,1 +1,12 @@
 extends CharacterState
+
+
+func _init():
+	self.behaviors = ["fall_behavior"]
+
+
+func enter(_options = {}):
+	var character = self._actor
+	character.get_node("collision").set_deferred("disabled", true)
+	character.get_node("hurtbox/collision").set_deferred("disabled", true)
+	character.set_deferred("velocity", Vector2(0, -400))
--- actors/character/states/HURT.gd
+++ actors/character/states/HURT.gd
@@ -7,16 +7,19 @@ func _init():
 
 func enter(_options = {}):
 	var character = self._actor
+	if character.damage_counter == 1:
+		self.change_state("DEAD")
+	character.damage_counter += 1
 	var back_direction = sign(character.velocity.x)
 	if back_direction == 0:
 		back_direction = -1
 
-	character.velocity.y -= character.JUMP_SPEED
-	character.velocity.x = character.SPEED * back_direction
+	character.velocity.y = -300
+	character.velocity.x = 80 * back_direction * -1
 
 
 func update(_delta):
 	var character = self._actor
 
-	if character.is_on_floor():
+	if character.velocity.y > 0 and character.is_on_floor():
 		self.change_state("IDLE")
