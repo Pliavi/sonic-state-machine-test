30b8cf1f0974f3499bde3d16551cc0e167948bf7
Pliavi <pliavi.rpgm@gmail.com>
2021-01-23 15:51:03 -0300

    tweaking walking behaviors speed changings

--- actors/character/Behaviors.gd
+++ actors/character/Behaviors.gd
@@ -1,22 +1,37 @@
-func walk_behavior(delta, target):
+func walk_behavior(delta, state: State):
+	var character = state._actor
 	var horizontal_direction = (
 		Input.get_action_strength("ui_right")
 		- Input.get_action_strength("ui_left")
 	)
-	var target_speed = target.SPEED * horizontal_direction * delta
+	var target_speed = character.SPEED * horizontal_direction * delta
 	var is_walk_pressed = abs(horizontal_direction)
-	var speed_decreasing_constant = 0.2 if is_walk_pressed else 0.1
+	var speed_changing_constant = 0.2
 
-	if horizontal_direction != 0:
-		target.global_scale.x = horizontal_direction
+	if is_walk_pressed and state.is_state("WALKING"):
+		speed_changing_constant = 0.05
 
-	target.velocity.x = lerp(
-		target.velocity.x, target_speed, speed_decreasing_constant
+	if state.is_state("JUMPING"):
+		speed_changing_constant = 0.05
+
+	if state.is_state("SPINNING"):
+		target_speed = 0  # character can't speedup while spinning
+		speed_changing_constant = 0.01
+		if character.will_skid():
+			speed_changing_constant = 0.2
+
+	if horizontal_direction != 0: # Change character direction
+		character.scale.x = horizontal_direction * sign(character.scale.y)  # https://github.com/godotengine/godot/issues/12335
+
+	character.velocity.x = lerp(
+		character.velocity.x, target_speed, speed_changing_constant
 	)
 
 
-func fall_behavior(delta: float, target: KinematicBody2D):
-	target.velocity.y += Globals.GRAVITY * delta
-	target.velocity.y = clamp(
-		target.velocity.y, -Globals.GRAVITY, Globals.GRAVITY
+func fall_behavior(delta: float, state: State):
+	var character = state._actor as KinematicBody2D
+
+	character.velocity.y += Globals.GRAVITY * delta
+	character.velocity.y = clamp(
+		character.velocity.y, -Globals.GRAVITY, Globals.GRAVITY
 	)
